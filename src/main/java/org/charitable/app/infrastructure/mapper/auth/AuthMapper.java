ae4650a9e31f3ba922bb1b4fb7b59f834614b475
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-11-05 17:04:50 +0545

    wip

--- src/main/java/org/charitable/app/infrastructure/mapper/auth/AuthMapper.java
+++ src/main/java/org/charitable/app/infrastructure/mapper/auth/AuthMapper.java
@@ -1,6 +1,9 @@
 package org.charitable.app.infrastructure.mapper.auth;
 
+import org.charitable.app.domain.entity.admin.Admin;
 import org.charitable.app.domain.entity.auth.Auth;
+import org.charitable.app.domain.entity.organization.Organization;
+import org.charitable.app.domain.entity.user.User;
 import org.charitable.app.infrastructure.adapter.outbound.jpa.auth.AuthEntity;
 import org.charitable.app.infrastructure.mapper.admin.AdminMapper;
 import org.charitable.app.infrastructure.mapper.organization.OrganizationMapper;
@@ -13,7 +16,31 @@ public class AuthMapper {
             return null;
         }
 
-        return getAuth(authEntity);
+        Organization orgDomain = authEntity.getOrganization() != null
+                ? OrganizationMapper.mapToDomain(authEntity.getOrganization())
+                : null;
+
+        Admin adminDomain = authEntity.getAdmin() != null
+                ? AdminMapper.mapToDomain(authEntity.getAdmin())
+                : null;
+
+        User userDomain = authEntity.getUser() != null
+                ? UserMapper.mapToDomain(authEntity.getUser())
+                : null;
+
+        return Auth.builder()
+                .id(authEntity.getId())
+                .createdAt(authEntity.getCreatedAt())
+                .updatedAt(authEntity.getUpdatedAt())
+                .email(authEntity.getEmail())
+                .phone(authEntity.getPhoneNumber())
+                .role(authEntity.getRole())
+                .isEmailVerified(authEntity.getIsEmailVerified())
+                .status(authEntity.getStatus())
+                .organization(orgDomain)
+                .admin(adminDomain)
+                .user(userDomain)
+                .build();
     }
 
     public static AuthEntity mapToEntity(Auth auth) {
@@ -38,22 +65,21 @@ public class AuthMapper {
         return entity;
     }
 
-
-    private static Auth getAuth(AuthEntity authEntity) {
-        var auth = new Auth(
-                authEntity.getEmail(),
-                null,
-                authEntity.getPhoneNumber(),
-                authEntity.getRole(),
-                authEntity.getIsEmailVerified(),
-                authEntity.getStatus(),
-                OrganizationMapper.mapToDomain(authEntity.getOrganization()),
-                UserMapper.mapToDomain(authEntity.getUser()),
-                AdminMapper.mapToDomain(authEntity.getAdmin())
-        );
-        auth.setId(authEntity.getId());
-        auth.setCreatedAt(authEntity.getCreatedAt());
-        auth.setUpdatedAt(authEntity.getUpdatedAt());
-        return auth;
-    }
+//    private static Auth getAuth(AuthEntity authEntity) {
+//        var auth = new Auth(
+//                authEntity.getEmail(),
+//                null,
+//                authEntity.getPhoneNumber(),
+//                authEntity.getRole(),
+//                authEntity.getIsEmailVerified(),
+//                authEntity.getStatus(),
+//                OrganizationMapper.mapToDomain(authEntity.getOrganization()),
+//                UserMapper.mapToDomain(authEntity.getUser()),
+//                AdminMapper.mapToDomain(authEntity.getAdmin())
+//        );
+//        auth.setId(authEntity.getId());
+//        auth.setCreatedAt(authEntity.getCreatedAt());
+//        auth.setUpdatedAt(authEntity.getUpdatedAt());
+//        return auth;
+//    }
 }
