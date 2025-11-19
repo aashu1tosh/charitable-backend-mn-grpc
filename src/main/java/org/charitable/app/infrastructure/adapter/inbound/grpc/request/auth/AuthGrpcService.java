e59d39ab73484c2610fbf3eafae3e5e3feea1c53
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-11-19 16:54:03 +0545

    fix: auth and refresh expiration time change

--- src/main/java/org/charitable/app/infrastructure/adapter/inbound/grpc/request/auth/AuthGrpcService.java
+++ src/main/java/org/charitable/app/infrastructure/adapter/inbound/grpc/request/auth/AuthGrpcService.java
@@ -64,13 +64,16 @@ public class AuthGrpcService extends AuthServiceGrpc.AuthServiceImplBase {
 
     @Override
     public void refreshToken(RefreshTokenRequest request, StreamObserver<RefreshTokenResponse> responseObserver) {
+        logger.info("Got Refresh Token request {}.",  request.getRefreshToken());
 
         var result = authUseCase.refreshToken(request.getRefreshToken());
-
         var responseToken = AuthTokenResponse.newBuilder()
                 .setAccessToken(result.getAccessToken())
                 .setRefreshToken(result.getRefreshToken())
                 .build();
+
+        logger.info("Received refresh token request for token: {}", responseToken.getRefreshToken());
+
         var response = RefreshTokenResponse.newBuilder()
                 .setSuccess(true)
                 .setMessage("Token refresh successfully")
